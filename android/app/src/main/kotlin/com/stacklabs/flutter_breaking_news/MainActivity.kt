5764af19a016d31d9e84b3da71cc067d61433265
bwnyasse <risbonyasse@gmail.com>
2020-04-06 00:21:28 -0400

    core(*): Init Project from flutter create cmd

---
+++ android/app/src/main/kotlin/com/stacklabs/flutter_breaking_news/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.stacklabs.flutter_breaking_news
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
