06618676ebf394fac1eed3fc3d9d8df974fe663a
bwnyasse <risbonyasse@gmail.com>
2020-04-11 15:29:45 -0400

    refacto(layout): reorganize app folder

--- lib/src/app/blocs/blocs.dart
+++ lib/src/app/blocs/blocs.dart
@@ -1,6 +1,6 @@
-export 'auth/auth_bloc.dart';
-export 'auth/auth_event.dart';
-export 'auth/auth_state.dart';
-export 'data/data_bloc.dart';
-export 'data/data_event.dart';
-export 'data/data_state.dart';
+export 'impl/auth/auth_bloc.dart';
+export 'impl/auth/auth_event.dart';
+export 'impl/auth/auth_state.dart';
+export 'impl/data/data_bloc.dart';
+export 'impl/data/data_event.dart';
+export 'impl/data/data_state.dart';
