09565cb2595942ce4337f88ae2c3ec3f5b5e6d65
bwnyasse <risbonyasse@gmail.com>
2020-04-13 11:46:37 -0400

    doc(license): rename copyright to license

--- license.sh
+++ license.sh
@@ -4,6 +4,6 @@ for i in $(find lib/ test/ -name '*.dart') # or whatever other pattern...
 do
   if ! grep -q Copyright $i
   then
-    cat copyright-header.txt $i >$i.new && mv $i.new $i
+    cat license.txt $i >$i.new && mv $i.new $i
   fi
 done
